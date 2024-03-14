1b08e6d0021b48484a838451f0f7c6ffdfdd9c02
jefrihillon <jefri.hillon@gmail.com>
2024-03-14 00:07:48 -0400

    After Starting Recipe1

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -22,6 +22,17 @@ class UserSerializer(serializers.ModelSerializer):
         """Create and return a user with encrypted passwords"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update and return user"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token"""
     email = serializers.EmailField()
@@ -43,5 +54,5 @@ class AuthTokenSerializer(serializers.Serializer):
             msg = _('Unable to authenitcate with provided credentials')
             raise serializers.ValidationError(msg, code='authorization')
 
-        attrs['users'] = user
+        attrs['user'] = user
         return attrs
